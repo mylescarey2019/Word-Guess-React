1598adb18edbc03006f4c87673703d95bf19f1bc
Myles Carey <seanmyles@comcast.net>
2020-07-16 13:53:24 -0700

    Updates

---
+++ precache-manifest.67869beffdd5af6eaa9407eb0b27edbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5a021f1088f6e1b783c6be9adba3f3",
+    "url": "/Word-Guess-React/index.html"
+  },
+  {
+    "revision": "2898e466860a35782ea1",
+    "url": "/Word-Guess-React/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "b8034aa46e9c59005545",
+    "url": "/Word-Guess-React/static/js/2.ef948c79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Word-Guess-React/static/js/2.ef948c79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2898e466860a35782ea1",
+    "url": "/Word-Guess-React/static/js/main.b7075683.chunk.js"
+  },
+  {
+    "revision": "fb5f19c1de3161b84749",
+    "url": "/Word-Guess-React/static/js/runtime-main.0b4c2b1e.js"
+  }
+]);
